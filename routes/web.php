e6310f9835ae10d02bc3ac9a59f4c14ee98530bf
25552011188-cmyk <25552011188@utb-univ.ac.id>
2026-06-26 23:27:33 +0700

    Add root route redirect to login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect('/login');
+});
 Route::get('/login', function () {
     return redirect('http://localhost:5173/login');
 })->name('login');
